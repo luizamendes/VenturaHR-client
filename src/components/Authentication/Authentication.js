40c1b5495d4ef36cedbab775e0f430cd2033475b
Luiza Mendes <luizaleitemendes@gmail.com>
2020-09-04 22:01:23 -0300

    Authentication component

---
+++ src/components/Authentication/Authentication.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Button } from "../Button";
+import { Input } from "../Input";
+import "./index.scss";
+
+export const Authentication = () => {
+  return (
+    <div className="authentication">
+      <div className="authentication__login">
+        <h3>Já tenho conta</h3>
+        <Input labelText="E-mail" type="text" />
+        <Input labelText="Senha" type="password" />
+      </div>
+      <div className="authentication__register">
+        <h3>Ainda não possuo conta</h3>
+        <Button buttonText="Criar conta" />
+      </div>
+    </div>
+  );
+};
