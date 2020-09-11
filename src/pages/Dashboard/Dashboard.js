59b63f420368166ed4c3a29b0566b1d425ea25ae
Luiza Mendes <luizaleitemendes@gmail.com>
2020-09-10 21:29:27 -0300

    Finishing dashboard page

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,11 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Cardbox } from "../../components/Cardbox";
 import { JobList } from "../../components/JobList";
+import { PageTitle } from "../../components/PageTitle";
+import { jobs } from "../../mocks/jobs";
 import "./index.scss";
 
 export const Dashboard = () => {
   return (
     <>
-      <h3>Dashboard</h3>
+      <PageTitle title="Dashboard" />
+      <div className="content-row">
+        <Cardbox title="Suas vagas">
+          {jobs.slice(0, 3).map((job) => (
+            <p>{job.job}</p>
+          ))}
+          <Link>Ver todas</Link>
+        </Cardbox>
+        <Cardbox title="Mensagens do sistema">
+          <p>Não há mensagens do sistema</p>
+        </Cardbox>
+      </div>
       <JobList />
     </>
   );
