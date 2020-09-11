79014a3a6ef9056e86cd8e9a61a6b1691daab9d5
Luiza Mendes <luizaleitemendes@gmail.com>
2020-09-10 21:28:54 -0300

    Cardbox component

---
+++ src/components/Cardbox/Cardbox.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./index.scss";
+
+export const Cardbox = ({ children, title, ...props }) => {
+  return (
+    <div className="cardbox" {...props}>
+      {title && <p className="cardbox__title">{title}</p>}
+      {children}
+    </div>
+  );
+};
