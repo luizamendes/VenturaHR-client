1c72d873ce4a7c94d6fefab24dc7b0ec6aad1b68
Luiza Mendes <luizaleitemendes@gmail.com>
2020-09-10 21:29:42 -0300

    Creating open position page

---
+++ src/pages/OpenPosition/OpenPosition.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { PageTitle } from "../../components/PageTitle";
+import "./index.scss";
+
+export const OpenPosition = () => {
+  return <PageTitle title="Criar vaga" />;
+};
